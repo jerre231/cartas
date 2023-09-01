e30fced31ad3cf1913b9f3708ea71d4f1e0bc1d6
jerre231 <joaoscofield.20231@poli.ufrj.br>
2023-09-01 16:16:47 -0300

    Back-done

--- classes.py
+++ classes.py
@@ -8,28 +8,19 @@ class Carta():
         self.msg = mensagem
         self.rmt = remetente
         
-        escrever = f"{self.dt}\n{self.dst}\n{self.msg}\n{self.rmt}"
+        self.escrever = f"{self.dt}\n{self.dst}\n{self.msg}\n{self.rmt}"
         
-        pdf = FPDF()
-        pdf_set_font("Arial", size = 12)
+        self.pdf = FPDF()
+        self.pdf.add_page() 
+        self.pdf.set_font("Arial", size = 12)
 
 
     def write(self):
         with open(f"carta_{self.id}.txt", 'w+') as arquivo:
-            arquivo.write(escrever)
+            arquivo.write(self.escrever)
 
-        pdf.cell(200, 10, txt = self.dt, ln = 1, align = 'C')
-        pdf.cell(200, 10, txt = self.dst, ln = 2, align = 'C')
-        pdf.cell(200, 10, txt = self.msg, ln = 3, align = 'C')
-        pdf.cell(200, 10, txt = self.rmt, ln = 4, align = 'C')
-        pdf.output(f"carta_{self.id}.pdf")
-
-class User():
-    def __init__(self, user, passw):
-        self.username = user
-        self.passw = passw
-
-    def cadastrar(self):
-        with open('users.txt', 'a') as arquivo:
-            linha = f"{self.username}: {self.passw}\n"
-            arquivo.write(linha)
+        self.pdf.cell(200, 10, txt = self.dt, ln = 1, align = 'C')
+        self.pdf.cell(200, 10, txt = self.dst, ln = 2, align = 'C')
+        self.pdf.cell(200, 10, txt = self.msg, ln = 3, align = 'C')
+        self.pdf.cell(200, 10, txt = self.rmt, ln = 4, align = 'C')
+        self.pdf.output(f"carta_{self.id}.pdf")
--- main.py
+++ main.py
@@ -1,47 +1,48 @@
-from flask import *
-import classes
-from datetime import date
+from flask import Flask, render_template, request, redirect
+from classes import Carta
+from datetime import datetime
 
 app = Flask(__name__)
 
-carta = []
-newl = len(carta)
-data_atual = date.today()
+cartas = []
+data_atual = datetime.now()
+
+credentials_dict = {}
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    error = None
+
+    with open('users.txt', 'r') as arquivo:
+        for linha in arquivo:
+            user, passw = linha.strip().split(": ")
+            credentials_dict[user] = passw
+
     if request.method == "POST":
-        user = request.form.get("nome")
-        global username
-        username = user
+        user = request.form.get("name")
         passw = request.form.get("password")
 
-        with open('users.txt', 'r') as arquivo:
-            linhas = arquivo.readlines()
+        if user in credentials_dict and credentials_dict[user] == passw:
+            return redirect(f"/carta/{user}")
+        else:
+            error = "Credenciais inválidas. Tente novamente."
 
-            for linha in linhas:
-                if linha.strip() == f"{user}: {passw}":
-                    return redirect(f"/carta/{username}")
+    return render_template("login.html", error=error)
 
-        return redirect("/login")
-
-    return render_template("login.html")
-
-@app.route("/carta/<username>", methods=['GET', 'POST'])
-def pagina_carta(username):
+@app.route("/carta/<user>", methods=['GET', 'POST'])
+def pagina_carta(user):
     if request.method == "POST":
-        
         data = data_atual.strftime("%d/%m/%Y, %H:%M:%S")
         mensagem = request.form.get("mensagem")
         destinatario = request.form.get("destinatario")
-        #remetente = request.form.get("remetente")
-        gerar_carta = request.form.get("gerar")
 
-        if gerar_carta:                 #Não sei qual condição coloca
-            carta.append(Carta(newl, data, destinatario, mensagem, username))
-            carta[newl].write()
-        
-        return render_template("carta.html")
+        if "gerar" in request.form:
+            newl = len(cartas)
+            carta = Carta(newl, data, destinatario, mensagem, user)
+            cartas.append(carta)
+            carta.write()
+
+    return render_template("carta.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
