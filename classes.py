339374aed373c47d07d05b2d8404869465e59db9
jerre231 <joaoscofield.20231@poli.ufrj.br>
2023-09-04 21:56:30 -0300

    classes_att

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 from fpdf import FPDF
+import os
 
 class Carta():
     def __init__(self, carta_id, data, destinatario, mensagem, remetente):
@@ -16,14 +17,14 @@ class Carta():
 
 
     def write(self):
-        with open(f"carta_{self.id}.txt", 'w+') as arquivo:
+        with open(f"banco_dt/carta_{self.id}.txt", 'w+') as arquivo:
             arquivo.write(self.escrever)
 
         self.pdf.cell(200, 10, txt = self.dt, ln = 1, align = 'C')
         self.pdf.cell(200, 10, txt = self.dst, ln = 2, align = 'C')
         self.pdf.cell(200, 10, txt = self.msg, ln = 3, align = 'C')
         self.pdf.cell(200, 10, txt = self.rmt, ln = 4, align = 'C')
-        self.pdf.output(f"carta_{self.id}.pdf")
+        self.pdf.output(f"banco_dt/carta_{self.id}.pdf")
 
 class User():
     def __init__(self, us, pw):
@@ -33,3 +34,15 @@ class User():
     def cadastrar(self):
         with open("users.txt", 'a') as arquivo:
             arquivo.write(f"{self.username}: {self.passw}")
+
+
+
+#sim, essa função é fora do User. Não tocar, obrigado.                                 #TODO: implementar uma janela em "cartas.html" para poder ler a "mensagem"!
+def encontrar_cartas(username):
+    for arquivo in os.listdir("banco_dt"):
+        if arquivo.endswith(".txt"):
+            with open(os.path.join("banco_dt", arquivo), "r") as txt:
+                nome_arquivo = txt.readline(2)
+
+                if nome_arquivo == username:
+                    mensagem = txt.readline(3)
